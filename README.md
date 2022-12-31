fdba791966a8b3f14a819e26b29e4ae3b2d7215a
Takumi Fukazawa <s21c1100zh@s.chibakoudai.jp>
2022-12-31 10:54:20 +0900

    Support integer only calculation

--- README.md
+++ README.md
@@ -37,13 +37,14 @@ talkerで送った文字列をlistenerに表示する。
 ## 必要なソフトウェア
 * Python
   * テスト済み: 3.7〜3.10
+* ROS2
 
 ## テスト環境
 * Ubuntu 22.04
 
 ## ライセンス
 * このソフトウェアパッケージは、3条項BSDライセンスの下、再配布および使用が許可されます。
-  * [LICENSE](https://github.com/Takumi-Fukazawa/mypkg/main/LICENSE)
+  * [LICENSE](https://github.com/Takumi-Fukazawa/mypkg/blob/master/LICENS)
 * このパッケージのコードは，下記のスライド（CC-BY-SA 4.0 by Ryuichi Ueda）のものを，本人の許可を得て自身の著作としたものです．
   * [ryuichiueda/my_slides robosys_2022](https://github.com/ryuichiueda/my_slides/tree/master/robosys_2022)
 * ©　2022 Takumi Fukazawa
--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-dire=~
-[ "$1" != "" ] && dir="$1"
+# SPDX-fileCopyrightText: 2022 Takumi Fukazawa
+# SPDX-License-Identifier: BSD-3-Clause
 
-cd $dir/ros2_ws
-colcon build
-sourc 4dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+ng ( ) {
+       echo NG at Line $1
+              res=1
+      }
+      res=0
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10 '
+      ### I/O TEST ###
+       in=$(ros2 run mypkg listener)
+      out=$(echo hi | ros2 run mypkg talker)
+      [ "${in}" = "[INFO] [1672446416.922124800] [listener]: Listen: person_msgs.msg.Person(message='hi', log=1)" ] || ng ${LINENO}
+
+       in=$(ros2 run mypkg listener)
+      out=$(echo やあ！ | ros2 run mypkg talker )  
+      [ "${in}" = "[INFO] [1672446457.499800800] [listener]: Listen: person_msgs.msg.Person(message='やあ！', log=1)" ] || ng ${LINENO}
+
+       in=$(ros2 run mypkg listener)
+      out=$(echo 2 | ros2 run mypkg talker) 
+      [ "${in}" ="[INFO] [1672446473.421415400] [listener]: Listen: person_msgs.msg.Person(message='2', log=1)" ] || ng ${LINENO}
+
+       in=$(ros2 run mypkg listener)
+      out=$( echo  | ros2 run mypkg talker)
+      [ "${in}" = "echo  | ros2 run mypkg talker" ] || ng ${LINENO}
+
+      [ "$res" = 0 ] && echo OK
+        exit $res
