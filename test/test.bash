8428ce860d7617352a406d6701d770be4e34ebbd
Takumi Fukazawa <s21c1100zh@s.chibakoudai.jp>
2023-01-01 11:32:34 +0900

    Support integer only calculation

--- test/test.bash
+++ test/test.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # SPDX-fileCopyrightText: 2022 Takumi Fukazawa
 # SPDX-License-Identifier: BSD-3-Clause
 
- dir=~
- [ "$1" != "" ] && dir="$1"
+dir=~
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
@@ -12,4 +11,4 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Listen: 10'
+grep 'log=10'
